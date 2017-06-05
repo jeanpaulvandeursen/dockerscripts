c20ab3fc8a5780c838a67fd473b34089ad8897ed
jeanpaulvandeursen <github@vandeursen.org>
2017-06-05 11:06:46 -0400

    initial

---
+++ angularrunner.sh
@@ -0,0 +1,3 @@
+docker run -d -p 80:8000 -v /root/dev/angular/HiAngular/app:/home/jp/dev/angular/HiAngular/app jeanpaulvandeursen/devangular bash -C /root/main.sh
+docker ps
+cd /root/dev/angular/HiAngular/app
